e7c97b451d43ca11182bb6a9a463b6105cb8695a
hossammoneer17-rgb <hossammoneer17@gmail.com>
2026-08-26 01:20:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// GitHub Pages -> Render backend URL. Replace this with your live Render URL.
+// Example: window.THREE_DO_API_URL = "https://three-do-list-api.onrender.com";
+window.THREE_DO_API_URL = "";
