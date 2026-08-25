98cf51d1a5443ea37fb7f554a2588fbf1613d07b
hossammoneer17-rgb <hossammoneer17@gmail.com>
2026-08-26 00:34:13 +0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// GitHub Pages -> Render backend URL. Replace this with your live Render URL.
-// Example: window.THREE_DO_API_URL = "https://three-do-list-api.onrender.com";
-window.THREE_DO_API_URL = "";
